6691d69a8483bf2264d970657b5364255459ce2b
Sumon <sumon@Sumons-Air.fritz.box>
2023-12-08 22:08:45 +0100

    OK

--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 01:04:14 by msumon            #+#    #+#             */
-/*   Updated: 2023/12/08 21:51:36 by msumon           ###   ########.fr       */
+/*   Updated: 2023/12/08 22:07:29 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -172,11 +172,21 @@ void sort_stack(t_stack **a, t_stack **b, int size)
 {
 	int	min;
 
-	while (size > 5)
+	while ((*a) && size > 5)
 	{
 		min = find_min(*a);
-		rotate_until_min(a, min);
-		pb(a, b);
+		if ((*a)->data == min)
+			pb(a, b);
+		else if ((*a)->next->data == min)
+		{
+			sa(a);
+			pb(a, b);
+		}
+		else
+		{
+			rotate_until_min(a, min);
+			pb(a, b);
+		}
 		size--;
 	}
 	if (size == 5 && is_sorted(*a) == 0)
