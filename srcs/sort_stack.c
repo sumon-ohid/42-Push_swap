e7271985bd6e26c6f6bcf8fbd07c375837962b84
Sumon <sumon@Sumons-Air.fritz.box>
2023-12-09 20:39:52 +0100

    OK

--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -6,36 +6,37 @@
 /*   By: msumon <msumon@student.42vienna.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 01:04:14 by msumon            #+#    #+#             */
-/*   Updated: 2023/12/09 17:46:46 by msumon           ###   ########.fr       */
+/*   Updated: 2023/12/09 20:36:29 by msumon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-int find_min(t_stack *stack)
+int find_min(t_stack *stack_min)
 {
 	int	min;
 
-	min = stack->data;
-	while (stack)
+	min = stack_min->data;
+	while (stack_min)
 	{
-		if (stack->data < min)
-			min = stack->data;
-		stack = stack->next;
+		if (stack_min->data < min)
+			min = stack_min->data;
+		stack_min = stack_min->next;
 	}
 	return (min);
+
 }
 
-int	find_max(t_stack *stack)
+int	find_max(t_stack *stack_max)
 {
 	int	max;
 
-	max = stack->data;
-	while (stack)
+	max = stack_max->data;
+	while (stack_max)
 	{
-		if (stack->data > max)
-			max = stack->data;
-		stack = stack->next;
+		if (stack_max->data > max)
+			max = stack_max->data;
+		stack_max = stack_max->next;
 	}
 	return (max);
 }
@@ -167,18 +168,25 @@ void	sort_five(t_stack **a, t_stack **b, int size)
 		pa(a, b);
 }
 
+t_stack *get_last_element(t_stack *stack_last)
+{
+	while (stack_last->next)
+		stack_last = stack_last->next;
+	return (stack_last);
+}
+
 void sort_stack(t_stack **a, t_stack **b, int size)
 {
 	int	min;
+	t_stack *last;
 
-	while ((*a) && size > 5)
+	while ((*a) && size > 5 && is_sorted(*a) == 0)
 	{
+		last = get_last_element(*a);
 		min = find_min(*a);
-		if ((*a)->data == min)
-			pb(a, b);
-		else if ((*a)->next->data == min)
+		if (last->data == min)
 		{
-			sa(a);
+			rra(a);
 			pb(a, b);
 		}
 		else
